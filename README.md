1693bfc48dfdabf3a8dce83708b2030cefd5927d
solouyv <solouyv@rambler.ru>
2021-02-28 22:35:31 +0300

    0.0.1

---
+++ README.md
@@ -0,0 +1,13 @@
+A simple python calculator.
+
+To install the program, use the command "pip install -e ."
+
+After installation, enter the following in the terminal: 
+    export FLASK_APP = web
+    flask init-db
+
+To run the calculator as a web application use the command: 
+    webpycalc 
+to work with the console version use: 
+    pycalc
+See "About" in the web application or "pycalc -h" in the terminal for details.
--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 
-from subprocess import check_call
 from setuptools import find_packages, setup
-from setuptools.command.install import install
 
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        check_call("export FLASK_APP=web".split())
-        check_call("flask init-db".split())
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='webpycalc',
-    version='1.0.0',
-    packages=find_packages(),
+    name="webpycalc-ROMAN-SOLOYOUV",
+    version="0.0.1",
+    author="Roman Soloyouv",
+    author_email="solouyv@gmail.com",
+    description="Pure-python web or command-line calculator.",
     include_package_data=True,
     zip_safe=False,
+    long_description=long_description,
+    url="https://github.com/solouyv/calculator",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     entry_points={
         'console_scripts': [
             'webpycalc = webpycalc:main',
             'pycalc = pycalc:main',
         ],
     },
-    cmdclass={
-        'install': PostInstallCommand,
-    },
+    packages=find_packages(),
+    python_requires=">=3.7",
     install_requires=[
         'flask',
     ],
